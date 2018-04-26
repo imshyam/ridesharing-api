f890912cf4e3f0a87c882027ba1e3ad884a38327
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-04-27 02:56:54 +0800

    configure routes and db

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require("express")
+const express = require("express");
+const mangoose = require('mongoose');
+
+const User = require('./models/userModel');
+const routes = require('./routes/userRoutes');
+
+const bodyParser = require('body-parser');
+
+mangoose.Promise = global.Promise;
+mangoose.connect('mangodb://localhost/UserDB');
 
 const app= express()
 
-app.get('/', (req, res) => res.send("Hello World"))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+routes(app);
+
 
 app.listen(3000, () => console.log("Listening to 3000!"))
