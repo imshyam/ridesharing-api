47b8180fb2c72069ce2e677dd8169b96ab7cfcf6
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-04-27 02:22:03 +0800

    implement methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,13 +5,32 @@ const mongoose = require('mongoose');
 const User = mongoose.model('Users');
 
 exports.try_signup = function(req, res) {
-    res.send("SignUp");
+    User.find({'username': req.params.username}, function(err, user){
+        if(err)
+            res.send("Username already exists.");
+        var user = new User(req.body);
+        res.json(user);
+    });
 };
 
 exports.try_signin = function(req, res) {
-    res.send("SignIn");
+    res.params.session_key = "apple";
+    User.find({'username': req.params.username, 
+               'password': req.params.password}, 
+                req.body, {new: true},function(err, user){
+        if(err)
+            res.send("Username or Password Don't Match.");
+        res.json(user);
+    });
 };
 
 exports.try_signout = function(req, res) {
-    res.send("SignOut");
+    User.find({'username': req.params.username, 
+               'session_key': req.params.session_key}, 
+               req.body, {new: true},function(err, user){
+        if(err)
+            res.send("You are Not Logged In.");
+        res.params.session_key = "EMPTY";
+        res.json(user);
+    });
 };
