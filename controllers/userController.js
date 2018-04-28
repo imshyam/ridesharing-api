8a4a91d7e8b5c27dc01861320d1725d2d49369da
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-04-29 01:28:28 +0800

    check for username

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,15 +5,20 @@ const mongoose = require('mongoose');
 const User = mongoose.model('Users');
 
 exports.try_signup = function(req, res) {
-    User.find({username: req.params.username}, function(err, user){
-        if(err)
-            res.send("Username already exists.");
-        var new_user = new User(req.body);
-        new_user.save(function(err, user) {
-            if (err)
-                res.send(err);
-            res.json(user);
-        });
+    User.find({username: req.body.username}, function(err, user){
+        if(err) {
+            res.send(err);
+        }
+        if(user.length > 0){
+            res.send("Username is taken.");
+        } else {
+            var new_user = new User(req.body);
+            new_user.save(function(err, usr) {
+                if (err)
+                    res.send(err);
+                res.json(usr);
+            });
+        }
     });
 };
 
